79d71758a68c265a86902af11028baecf8ac4317
pzmcz <169125723+pzmcz@users.noreply.github.com>
2024-07-28 14:55:42 +0100

    Dev publish

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 // Localises the src for require('prc_api')
+module.exports = require('./src/id.js')
---
+++ src/id.js
@@ -0,0 +1,7 @@
+const init = async function(ERLC_API_KEY) {
+    console.log(ERLC_API_KEY)
+}
+
+module.exports = {
+    init
+}
